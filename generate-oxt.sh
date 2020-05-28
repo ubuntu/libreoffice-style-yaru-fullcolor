37f2fbb00f20fff72706f27461070736f45faf1d
Jupiter007 <paul.kepinski@orange.fr>
2020-05-28 11:49:41 +0200

    Update gen script

--- generate-oxt.sh
+++ generate-oxt.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+cd "build"
+
+echo "Zip icons"
+
+zip -r -q "images_yaru.zip" *
+
+cd "../"
+
 cp "build/images_yaru.zip" \
 "oxt/iconsets/images_yaru.zip"
 
 cd "oxt"
 
+echo "Create oxt"
+
 zip -r "yaru-theme.zip" *
 
 mv "yaru-theme.zip" "../yaru-theme.oxt"
+
+echo "Finish"
