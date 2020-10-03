b90a23c67ae8bcdb593927dc047b253cb58d0c39
Jupiter007 <paul.kepinski@orange.fr>
2020-10-03 14:05:18 +0200

    Check build/***/links.txt files

--- check-links.sh
+++ check-links.sh
@@ -14,55 +14,75 @@
 # You should have received a copy of the GNU Lesser General Public License along with
 # this program; if not, see <https://www.gnu.org/licenses/lgpl-3.0.txt>
 
-linksfile='src/links.txt'
-n=1
-errors=0
-linkedicons=()
-targeticons=()
+function check-links() {
+    linksfile=$1
+    verbose=$2
+    n=1
+    errors=0
+    linkedicons=()
+    targeticons=()
 
-while read line; do
-    if [ "$line" = "" ] || [[ "$line" =~ ^#.*  ]]
-    then
-        continue
-    fi
+    while read line; do
+        if [ "$line" = "" ] || [[ "$line" =~ ^#.*  ]]
+        then
+            continue
+        fi
 
-    IFS=' '
-    read -ra splitedline <<< "$line"
-    if [[ ${#splitedline[@]} > 2 ]] || [[ ${#splitedline[@]} < 2 ]]; then
-        echo "Error line $n: Malformed line '$line'"
-        let errors+=1
-    else
-        linkedicons+=(${splitedline[0]})
-        targeticons+=(${splitedline[1]})
-    fi
+        IFS=' '
+        read -ra splitedline <<< "$line"
+        if [[ ${#splitedline[@]} > 2 ]] || [[ ${#splitedline[@]} < 2 ]]; then
+            echo "Error line $n: Malformed line '$line'"
+            let errors+=1
+        else
+            linkedicons+=(${splitedline[0]})
+            targeticons+=(${splitedline[1]})
+        fi
 
-    let n+=1
-done < $linksfile
+        let n+=1
+    done < $linksfile
 
-n=1
+    n=1
 
-for i in "${targeticons[@]}"
-do
-    if [[ " ${linkedicons[@]} " =~ " ${i} " ]]; then
+    for i in "${targeticons[@]}"
+    do
+        if [[ " ${linkedicons[@]} " =~ " ${i} " ]]; then
 
-        linkediconindex=
-        for j in "${!linkedicons[@]}"; do
-            if [[ "${linkedicons[$j]}" = "${i}" ]]; then
-               linkediconindex=$j
-               break
-           fi
-        done
+            linkediconindex=
+            for j in "${!linkedicons[@]}"; do
+                if [[ "${linkedicons[$j]}" = "${i}" ]]; then
+                   linkediconindex=$j
+                   break
+               fi
+            done
 
-        echo "Error line $n: Link ${linkedicons[n-1]} -> $i -> ${targeticons[linkediconindex]}"
+            echo "Error line $n: Link ${linkedicons[n-1]} -> $i -> ${targeticons[linkediconindex]}"
 
-        let errors+=1
-    fi
-    let n+=1
-done
+            let errors+=1
+        fi
+        let n+=1
+    done
 
-if [[ $errors > 0 ]]; then
-    echo -e "\n$errors error(s) found"
+    return $errors
+}
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo -e "\n=> ⏳ Checking src/links.txt - please wait"
+
+check-links "src/links.txt"
+
+if [[ $? > 0 ]]; then
+    echo -e "\n=> $errors error(s) found\n"
     exit 1
 else
-    echo "🎉 0 error found"
+    echo -e "\n=> ⏳ Checking build/***/links.txt - please wait"
+
+    if [[ $(check-links 'build/svg/links.txt') > 0 ]] || [[ $(check-links 'build/png/links.txt') > 0 ]]; then
+        echo -e "\n=> Errors errors found into /build links files - please run ${bold}./generate-links.sh${normal} to regenerate them\n"
+        exit 1
+    else
+        echo -e "\n=> 🎉 0 error found\n"
+        exit 0
+    fi
 fi
