3b91c3a1f44c2a221118242fced7dbc23653ac5d
Jupiter007 <paul.kepinski@orange.fr>
2020-11-24 17:18:34 +0100

    Enhancement of the build script
    - Use CLInt to parsing flags and arguments
    - Add a watch function
    - Rebuild links (was outdated)

--- build.sh
+++ build.sh
@@ -14,6 +14,15 @@
 # You should have received a copy of the GNU Lesser General Public License along with
 # this program; if not, see <https://www.gnu.org/licenses/lgpl-3.0.txt>
 
+## Build script
+##
+## usage: ./build.sh [options]
+##
+## options:
+##      -f, --file <string>  Svg file path to build. Must looks like "/cmd/sc_singlepage" (without file ext and /src prefix)
+##      -a, --all            Delete the /build folder and recreate it entirely from /src [default: 0]
+##      -w, --watch          Watch file changes () [default: 0]
+
 echo
 
 if ! command -v inkscape >/dev/null
@@ -34,11 +43,73 @@ then
     exit 1
 fi
 
-if [[ -z $1 ]]
+if ! command -v inotifywait >/dev/null
 then
-    echo  -e "=> 🙅 Please give a valid parameter\n"
+    echo  -e "=> 🙅 Please install inotify-tools\n"
     exit 1
-elif [[ $1 = "--all" ]]
+fi
+
+# CLInt GENERATED_CODE: start
+# Default values
+_all=0
+_watch=0
+
+# No-arguments is not allowed
+[ $# -eq 0 ] && sed -ne 's/^## \(.*\)/\1/p' $0 && exit 1
+
+# Converting long-options into short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+"--file") set -- "$@" "-f";;
+"--all") set -- "$@" "-a";;
+"--watch") set -- "$@" "-w";;
+  *) set -- "$@" "$arg"
+  esac
+done
+
+function print_illegal() {
+    echo Unexpected flag in command line \"$@\"
+}
+
+# Parsing flags and arguments
+while getopts 'hawf:' OPT; do
+    case $OPT in
+        h) sed -ne 's/^## \(.*\)/\1/p' $0
+           exit 1 ;;
+        a) _all=1 ;;
+        w) _watch=1 ;;
+        f) _file=$OPTARG ;;
+        \?) print_illegal $@ >&2;
+            echo "---"
+            sed -ne 's/^## \(.*\)/\1/p' $0
+            exit 1
+            ;;
+    esac
+done
+# CLInt GENERATED_CODE: end
+
+function render_icon() {
+    echo -e "=> 🌠 Copy links.txt\n"
+
+    cp -f "./src/links.txt" "./build/png/links.txt"
+    sed -i 's/.xxx/.png/g' "./build/png/links.txt"
+
+    cp -f "./src/links.txt" "./build/svg/links.txt"
+    sed -i 's/.xxx/.svg/g' "./build/svg/links.txt"
+
+    echo -e "=> 🔨 Render PNG file\n"
+    inkscape -o "./build/png${1}.png" "./src${1}.svg"
+
+    echo -e "\n=> ✨ Optimize PNG\n"
+    optipng -o7 "./build/png${1}.png"
+
+    echo -e "\n=> ✨ Minimify SVG"
+
+    svgo -i "./src${1}.svg" -o "./build/svg${1}.svg"
+}
+
+if [[ $_all = 1 ]];
 then
     echo -e "=> 🔥 Warning this will delete the /build folder and recreate it entirely from /src (will take a while)\n"
     read -p "=> Continue? (yes/no) " continue
@@ -81,22 +152,22 @@ then
 
     echo -e "\n=> ✨ Minimify all SVG ...\n"
     svgo -r -f svg
+
+elif [[ $_watch = 1 ]];
+then
+    echo -e "=> 🔍 Lets watch the files ...\n"
+
+    while true; do
+        filename=$(inotifywait -r -q --event close_write --format %w%f src/)
+        if [[ $filename == *.svg ]]; then
+            filename=${filename#"src"}
+            filename=${filename%".svg"}
+
+            render_icon $filename
+
+            echo
+        fi
+    done
 else
-    echo -e "=> 🌠 Copy links.txt\n"
-
-    cp -f "./src/links.txt" "./build/png/links.txt"
-    sed -i 's/.xxx/.png/g' "./build/png/links.txt"
-
-    cp -f "./src/links.txt" "./build/svg/links.txt"
-    sed -i 's/.xxx/.svg/g' "./build/svg/links.txt"
-
-    echo -e "=> 🔨 Render PNG file\n"
-    inkscape -o "./build/png${1}.png" "./src${1}.svg"
-
-    echo -e "\n=> ✨ Optimize PNG\n"
-    optipng -o7 "./build/png${1}.png"
-
-    echo -e "\n=> ✨ Minimify SVG"
-
-    svgo -i "./src${1}.svg" -o "./build/svg${1}.svg"
+    render_icon $_file
 fi
